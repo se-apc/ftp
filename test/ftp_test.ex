d5813e54f36edf8b94f57957dc60994b12f26bc9
alan.jackson <alan.jackson@schneider-electric.com>
2019-01-23 11:27:10 +0000

    run formatter

--- test/ftp_test.ex
+++ test/ftp_test.ex
@@ -1,89 +1,87 @@
 defmodule FtpTest do
-    
-    use ExUnit.Case, async: false
+  use ExUnit.Case, async: false
 
-    @server_name :test_server
-    @test_addr "127.0.0.1"
-    @test_port 7701
-    @max_connections 50
-  
-    test "Try to connect max_connections times, expect all connections to be successful" do
-        start_server()
+  @server_name :test_server
+  @test_addr "127.0.0.1"
+  @test_port 7701
+  @max_connections 50
 
-        ## Will return a list containing value of {:ok, pid} or {:error, error}
-        results = 
-        for _x <- 1..@max_connections, do: :ftp.open(to_charlist(@test_addr), @test_port)
+  test "Try to connect max_connections times, expect all connections to be successful" do
+    start_server()
 
-        ## actual_results will be a list of booleans
-        {ftp_pids, actual_results} = analyse_results(results)
+    ## Will return a list containing value of {:ok, pid} or {:error, error}
+    results = for _x <- 1..@max_connections, do: :ftp.open(to_charlist(@test_addr), @test_port)
 
-        close_ftp_connections(ftp_pids)
+    ## actual_results will be a list of booleans
+    {ftp_pids, actual_results} = analyse_results(results)
 
-        expected_results = for _x <- 1..@max_connections, do: true
+    close_ftp_connections(ftp_pids)
 
-        stop_server()
+    expected_results = for _x <- 1..@max_connections, do: true
 
-        assert expected_results == actual_results
-    end
-  
-    test "Try to connect max_connections+1 times, expect the last time to fail" do
-        start_server()
+    stop_server()
 
-        ## Will return a list containing value of {:ok, pid} or {:error, error}
-        results = 
-        for _x <- 1..@max_connections+1, do: :ftp.open(to_charlist(@test_addr), @test_port)
+    assert expected_results == actual_results
+  end
 
-        ## actual_results will be a list of booleans
-        {ftp_pids, actual_results} = analyse_results(results)
+  test "Try to connect max_connections+1 times, expect the last time to fail" do
+    start_server()
 
-        close_ftp_connections(ftp_pids)
+    ## Will return a list containing value of {:ok, pid} or {:error, error}
+    results =
+      for _x <- 1..(@max_connections + 1), do: :ftp.open(to_charlist(@test_addr), @test_port)
 
-        expected_results = for _x <- 1..@max_connections, do: true
-        expected_results = expected_results ++ [false] ## add last case as a failure
+    ## actual_results will be a list of booleans
+    {ftp_pids, actual_results} = analyse_results(results)
 
-        stop_server()
+    close_ftp_connections(ftp_pids)
 
-        assert expected_results == actual_results
-    end
+    expected_results = for _x <- 1..@max_connections, do: true
+    ## add last case as a failure
+    expected_results = expected_results ++ [false]
 
+    stop_server()
 
-    ## =========================================================== ##
-    ## SOME HELPER FUNCTIONS
-    ## =========================================================== ##
+    assert expected_results == actual_results
+  end
 
-    defp analyse_results(results) do
-        boolean_results = 
-        for result <- results do
-            case result do
-                {:ok, _pid} -> true
-                {:error, _} -> false
-            end 
+  ## =========================================================== ##
+  ## SOME HELPER FUNCTIONS
+  ## =========================================================== ##
+
+  defp analyse_results(results) do
+    boolean_results =
+      for result <- results do
+        case result do
+          {:ok, _pid} -> true
+          {:error, _} -> false
         end
+      end
 
-        {results, boolean_results}
-    end
+    {results, boolean_results}
+  end
 
-    ## close ftp connections from client side
-    defp close_ftp_connections(ftp_connections) do
-        for ftp_connection <- ftp_connections, do: close_ftp_connection(ftp_connection)
-    end
-    defp close_ftp_connection({:ok, pid}), do: :ftp.close(pid)
-    defp close_ftp_connection({:error, _}), do: :ok
+  ## close ftp connections from client side
+  defp close_ftp_connections(ftp_connections) do
+    for ftp_connection <- ftp_connections, do: close_ftp_connection(ftp_connection)
+  end
 
-    defp start_server() do
-        opts = [
-            username: "user",
-            password: "pass",
-            max_connections: @max_connections
-          ]
-      
-        root = Path.absname("") <> "/tmp/ftp_root"
-        File.mkdir_p!(root)
-        Ftp.start_server(@server_name, @test_addr, @test_port, root, opts)
-    end
+  defp close_ftp_connection({:ok, pid}), do: :ftp.close(pid)
+  defp close_ftp_connection({:error, _}), do: :ok
 
-    defp stop_server() do
-        Ftp.close_server(@server_name)
-    end
+  defp start_server() do
+    opts = [
+      username: "user",
+      password: "pass",
+      max_connections: @max_connections
+    ]
+
+    root = Path.absname("") <> "/tmp/ftp_root"
+    File.mkdir_p!(root)
+    Ftp.start_server(@server_name, @test_addr, @test_port, root, opts)
+  end
+
+  defp stop_server() do
+    Ftp.close_server(@server_name)
+  end
 end
-  
