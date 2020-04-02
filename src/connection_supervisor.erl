6fd4716e7215627428bc8c757e8b39cdd5f4ece6
alan.jackson <alan.jackson@schneider-electric.com>
2020-04-02 18:36:05 +0000

    Switch to erlang:monitor/2
    process_flag/2 does actually increase CPU. erlang:monitor/2 does not.

--- src/connection_supervisor.erl
+++ src/connection_supervisor.erl
@@ -11,7 +11,7 @@ start_link([Args]) ->
     gen_server:start_link(?MODULE, Args, []).
 
 init(InitialState) ->
-    process_flag(trap_exit, true),
+    erlang:monitor(process, self()),
     {ok, InitialState}.  
 
 handle_call(_Request, _From, State) ->
@@ -25,13 +25,13 @@ handle_info({new_connection, Acceptor, Socket}, InitialState) ->
             Acceptor ! {ack, Worker},
     {noreply, InitialState};
 
-handle_info({'EXIT', _Pid, normal}, State) ->
+handle_info({'DOWN', _Ref, process, _Pid, normal}, State) ->
     {noreply, State};
 
-handle_info({'EXIT', _Pid, shutdown}, State) ->
+handle_info({'DOWN', _Ref, process, _Pid, shutdown}, State) ->
     {noreply, State};
 
-handle_info({'EXIT', Pid, Info}, State) ->
+handle_info({'DOWN', _Ref, process, Pid, Info}, State) ->
     error_logger:error_msg("Control connection ~p crashed: ~p~n", [Pid, Info]),
     {noreply, State};
 
