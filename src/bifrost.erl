60f10172e3c1b39d3c2bd0ef06acd1138aa3616e
alan.jackson <alan.jackson@schneider-electric.com>
2020-04-02 14:38:18 +0100

    remove compile error

--- src/bifrost.erl
+++ src/bifrost.erl
@@ -10,7 +10,7 @@
 -include("bifrost.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--export([start_link/2, establish_control_connection/2, await_connections/2, supervise_connections/1]).
+-export([start_link/2, establish_control_connection/2, await_connections/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
