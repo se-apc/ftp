f38d195260d1a10f913f43677e76953c4761f595
alanj853 <alanjackson.nuig@gmail.com>
2021-01-29 09:47:55 +0000

    add debug info

--- lib/ftp/session_refresher.ex
+++ lib/ftp/session_refresher.ex
@@ -6,7 +6,8 @@ defmodule Ftp.SessionRefresher do
     def start_link(_), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
     def init(_), do: {:ok, %{}}
     def handle_info({:refresh_session, module_state}, state) do
-        Logger.warn("Refreshing for data transfer...")
+        session = Map.get(module_state, :session)
+        Logger.info("Refreshing for data transfer #{inspect session}...")
         Ftp.Bifrost.refresh_session(module_state)
         {:noreply, state}
     end
